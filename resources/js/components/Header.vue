27721abfbb841c9e888900cc05540e40b3d9dbed
Niyaz <jniyaz@gmail.com>
2019-10-28 11:25:52 +0800

    Project initial commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="header">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    App Name
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="content">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Home</div>
+
+                        <div class="card-body">
+                            {{ welcome }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        computed: {
+            welcome() {
+                return this.$store.getters.welcome;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #content{
+        margin-top: 20px;
+    }
+</style>
